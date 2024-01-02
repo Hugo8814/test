f1bd09679ab989bf46955d2d2d17e65011c727ee
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-01-02 23:21:15 +0000

    day 2

---
+++ starter/script.js
@@ -0,0 +1,8 @@
+// import { addtoCart, totalPrice, tq } from './shoppingCart.js';
+// console.log('improting');
+// addtoCart('bread', 5);
+// console.log(totalPrice, tq);
+
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addtoCart('bread', 4);
