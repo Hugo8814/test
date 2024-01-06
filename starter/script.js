9a26e7d149676d265807cd430c21dd9e28b38e9a
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-01-06 23:14:50 +0000

    done, next

--- starter/script.js
+++ starter/script.js
@@ -8,9 +8,23 @@
 // console.log(ShoppingCart.totalPrice);
 
 // import add, { addtoCart, totalPrice as price, tq } from './shoppingCart.js';
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 3);
-add('pizza', 1);
-console.log(cart);
-console.log(add);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 3);
+// add('pizza', 1);
+// console.log(cart);
+// console.log(add);
+// console.log('start of something');
+
+// console.log(data);
+// console.log('end something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1) };
+};
+
+getLastPost();
