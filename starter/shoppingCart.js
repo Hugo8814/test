e38bb0e51d82f4fca720f9329f5e31b3e7b7692b
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-01-12 20:59:18 +0000

    done, next

--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -6,18 +6,18 @@
 
 console.log('exporting');
 
-const shoppingCost = 10;
-export const cart = [];
+// const shoppingCost = 10;
+// export const cart = [];
 
-export const addtoCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
-};
-const totalPrice = 234;
-const totalquntity = 23;
-export { totalPrice, totalquntity as tq };
+// export const addtoCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+// const totalPrice = 234;
+// const totalquntity = 23;
+// export { totalPrice, totalquntity as tq };
 
-export default function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
-}
+// export default function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// }
