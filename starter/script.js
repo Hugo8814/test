8509abf9a8418ef662bcca512c058fe650bfa81e
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-01-18 20:02:26 +0000

    i dont even know any more

--- starter/script.js
+++ starter/script.js
@@ -1,70 +1,71 @@
-// import { addtoCart, totalPrice, tq } from './shoppingCart.js';
-// console.log('improting');
-// addtoCart('bread', 5);
-// console.log(totalPrice, tq);
-//import * as ShoppingCart from './shoppingCart.js';
+import { addtoCart, totalPrice, tq } from './shoppingCart.js';
+console.log('improting');
+addtoCart('bread', 5);
+console.log(totalPrice, tq);
+import * as ShoppingCart from './shoppingCart.js';
 
-// ShoppingCart.addtoCart('bread', 4);
-// console.log(ShoppingCart.totalPrice);
+ShoppingCart.addtoCart('bread', 4);
+console.log(ShoppingCart.totalPrice);
 
-// import add, { addtoCart, totalPrice as price, tq } from './shoppingCart.js';
-// import add, { cart } from './shoppingCart.js';
-// add('pizza', 2);
-// add('bread', 3);
-// add('pizza', 1);
-// console.log(cart);
-// console.log(add);
-// console.log('start of something');
+import add, { addtoCart, totalPrice, tq } from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 3);
+add('pizza', 1);
+console.log(cart);
+console.log(add);
+console.log('start of something');
 
-// console.log(data);
-// // console.log('end something');
+//console.log(data);
+// console.log('end something');
 
-// const getLastPost = async function () {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-//   const data = await res.json();
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
 
-//   return { title: data.at(-1), text: data.at(-1).body };
-// };
+  return { title: data.at(-1), text: data.at(-1).body };
+};
 
-// const lastpost = getLastPost();
+const lastpost = getLastPost();
 
-// console.log(lastpost);
+console.log(lastpost);
 
-// // not very clean
-// //lastpost.then(last => console.log(last));
+// not very clean
+//lastpost.then(last => console.log(last));
 
 // const lastPost2 = await getLastPost();
 // console.log(lastPost2);
 
-// const shoppingCart2 = (function () {
-//   const cart = [];
-//   const shippingCost = 10;
-//   const totalPrice = 237;
-//   const totalquntity = 23;
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalquntity = 23;
 
-//   const addtoCart = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(
-//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
-//     );
-//   };
-//   const orderStock = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} oreder from suppiler `);
-//   };
-//   return {
-//     addtoCart,
-//     cart,
-//     totalPrice,
-//     totalquntity,
-//   };
-// })();
+  const addtoCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} oreder from suppiler `);
+  };
+  return {
+    addtoCart,
+    cart,
+    totalPrice,
+    totalquntity,
+  };
+})();
 
-// shoppingCart2.addtoCart('apple', 3);
-// shoppingCart2.addtoCart('pizza', 2);
-// console.log(shoppingCart2);
+shoppingCart2.addtoCart('apple', 3);
+shoppingCart2.addtoCart('pizza', 2);
+console.log(shoppingCart2);
 
-import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+// import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
 
 const state = {
   cart: [
@@ -81,3 +82,6 @@ state.user.loggedIn = false;
 
 console.log(stateClone);
 console.log(stateDeepClone);
+if (module.hot) {
+  module.hot.accept();
+}
--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -1,23 +1,21 @@
-// exporting
-//bolcking code
-// console.log('blocking code2');
-// await fetch('https://jsonplaceholder.typicode.com/posts');
-// console.log('blocking code2');
+console.log('blocking code2');
+fetch('https://jsonplaceholder.typicode.com/posts');
+console.log('blocking code2');
 
 console.log('exporting');
 
-// const shoppingCost = 10;
-// export const cart = [];
+const shoppingCost = 10;
+export const cart = [];
 
-// export const addtoCart = function (product, quantity) {
-//   cart.push({ product, quantity });
-//   console.log(`${quantity} ${product} added to cart`);
-// };
-// const totalPrice = 234;
-// const totalquntity = 23;
-// export { totalPrice, totalquntity as tq };
+export const addtoCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+const totalPrice = 234;
+const totalquntity = 23;
+export { totalPrice, totalquntity as tq };
 
-// export default function (product, quantity) {
-//   cart.push({ product, quantity });
-//   console.log(`${quantity} ${product} added to cart`);
-// }
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
